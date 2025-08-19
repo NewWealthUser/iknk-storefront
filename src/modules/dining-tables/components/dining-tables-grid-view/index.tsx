c05eabb8bc060dd504b958fce2573a0cf0350381
[dyad] <camnewwealth@gmail.com>
2025-08-19 11:50:15 +0200

    [dyad] Made product grid full-width - wrote 1 file(s)

--- src/modules/dining-tables/components/dining-tables-grid-view/index.tsx
+++ src/modules/dining-tables/components/dining-tables-grid-view/index.tsx
@@ -46,49 +46,47 @@ const DiningTablesGridView = () => {
   return (
     <div>
       <DiningTablesHeader viewMode={viewMode} setViewMode={setViewMode} />
-      <div className="content-container">
-        <div className="lg:pb-12 pb-6 sm:pb-8 md:pb-10">
-          <div id="component-product-grid" className="relative">
-            <div className={gridClasses}>
-              {products.map((product, index) => (
-                <div key={index} className="flex w-full justify-center">
-                  <div className="relative flex h-full w-full flex-col">
-                    <div className="group/item group relative flex flex-col items-end justify-center" style={{ touchAction: 'pan-y', height: 'auto' }}>
-                      <div className="embla group/item group relative z-10 block w-full overflow-hidden">
-                        <div className="embla__container flex h-full items-center">
-                          <div className="embla__slide relative z-20 min-w-0 flex-[0_0_100%] justify-around">
-                            <a className="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover cursor-pointer css-7qu4t5" href={product.href}>
-                              <div className="relative h-full w-full grid content-end aspect-[1/1]">
-                                <img src={product.imgSrc} loading="lazy" alt={product.alt} className="h-full w-full opacity-1 mx-auto grid content-end object-contain" />
-                              </div>
-                            </a>
-                          </div>
+      <div className="lg:pb-12 pb-6 sm:pb-8 md:pb-10 px-4 sm:px-6 md:px-8">
+        <div id="component-product-grid" className="relative">
+          <div className={gridClasses}>
+            {products.map((product, index) => (
+              <div key={index} className="flex w-full justify-center">
+                <div className="relative flex h-full w-full flex-col">
+                  <div className="group/item group relative flex flex-col items-end justify-center" style={{ touchAction: 'pan-y', height: 'auto' }}>
+                    <div className="embla group/item group relative z-10 block w-full overflow-hidden">
+                      <div className="embla__container flex h-full items-center">
+                        <div className="embla__slide relative z-20 min-w-0 flex-[0_0_100%] justify-around">
+                          <a className="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover cursor-pointer css-7qu4t5" href={product.href}>
+                            <div className="relative h-full w-full grid content-end aspect-[1/1]">
+                              <img src={product.imgSrc} loading="lazy" alt={product.alt} className="h-full w-full opacity-1 mx-auto grid content-end object-contain" />
+                            </div>
+                          </a>
                         </div>
                       </div>
                     </div>
-                    <div className="flex h-full w-full flex-col flex-wrap content-around">
-                      <div className="flex h-full flex-col">
-                        <a className="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover css-7qu4t5" href={product.href}>
-                          <p className="text-xs text-gray-500 text-center pt-2" style={{ minHeight: '28px' }}>Available in multiple finishes </p>
-                          <div className="flex flex-col">
-                            <div className="mt-2">
-                              <div className="uppercase text-center">
-                                <span className="text-sm uppercase text-center font-semibold text-gray-800">{product.name}</span>
-                              </div>
-                            </div>
-                            <div className="mt-1 flex justify-center flex-wrap items-baseline text-xs">
-                              <span className="text-gray-600 mr-1">Starting at</span>
-                              <span className="text-black font-medium mr-1">{product.priceMember} Member /</span>
-                              <span className="text-gray-600">{product.priceRegular} Regular</span>
+                  </div>
+                  <div className="flex h-full w-full flex-col flex-wrap content-around">
+                    <div className="flex h-full flex-col">
+                      <a className="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover css-7qu4t5" href={product.href}>
+                        <p className="text-xs text-gray-500 text-center pt-2" style={{ minHeight: '28px' }}>Available in multiple finishes </p>
+                        <div className="flex flex-col">
+                          <div className="mt-2">
+                            <div className="uppercase text-center">
+                              <span className="text-sm uppercase text-center font-semibold text-gray-800">{product.name}</span>
                             </div>
                           </div>
-                        </a>
-                      </div>
+                          <div className="mt-1 flex justify-center flex-wrap items-baseline text-xs">
+                            <span className="text-gray-600 mr-1">Starting at</span>
+                            <span className="text-black font-medium mr-1">{product.priceMember} Member /</span>
+                            <span className="text-gray-600">{product.priceRegular} Regular</span>
+                          </div>
+                        </div>
+                      </a>
                     </div>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
